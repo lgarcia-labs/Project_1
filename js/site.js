61a77a60c9566039cf60d8d3c7bec5ac82dc7eb1
Leonel Garcia <lgarcia2961@gmail.com>
2025-03-06 21:27:49 -0600

    made it so it would great you when you submit the form

--- js/site.js
+++ js/site.js
@@ -1,3 +1,8 @@
-setTimeout(function() {
-  document.getElementById("submit-button").click();
-}, 3000);  // This will trigger the submit button after 3 seconds
+etTimeout(function() {
+      // Simulate the button click
+      document.getElementById("submit-button").click();
+      
+      // Show the "Thanks for signing up" message
+      document.getElementById("feedback").style.color = "green";
+      document.getElementById("feedback").textContent = "Thanks for signing up! and welcome to yugioh duel links";
+    }, 3000);  // This will click the submit button after 3 secondss
